455995684bb3407dd4fe7a5058a08f14ea724d62
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-07 14:27:45 +0200

    Creating Active button transition

--- NewestWebsite/application.js
+++ NewestWebsite/application.js
@@ -1,3 +1,15 @@
 const sections = document.querySelectorAll('.section');
 const sectButtons = document.querySelectorAll('.controlls');
 const sectButton = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content')
+
+function transition() {
+    for(let i = 0; i < sectButton.length; i++) {
+        sectButton[i].addEventListener('click', function() {
+            let currentButton = document.querySelectorAll('.active-button');
+            currentButton[0].className = currentButton[0].className.replace('active-button', '');
+            this.className += ' active-button'; //refering to this function
+        })
+    }
+}
+transition()();
