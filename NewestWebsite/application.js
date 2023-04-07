36933c744cbee72517ce910d225440d652ab686c
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-07 16:25:41 +0200

    Styling Home

--- NewestWebsite/application.js
+++ NewestWebsite/application.js
@@ -12,4 +12,23 @@ function transition() {
         })
     }
 }
+
+allSections.addEventListener('click', (e) =>{
+    const id = e.target.dataset.id;
+    if(id){//removes selected from other buttons
+        sectButtons.forEach((btn) => {
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        sections.forEach((section) => {
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+}) 
+
+
 transition()();
